88ec340c7c55d9e11f5a050f3ac7163df903f9a3
n.semenchuk <n.semenchuk@itransition.com>
2023-02-26 20:24:08 +0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,11 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const PORT = process.env.PORT || 7071
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:3000'],
+    },
+  })
 
   app.useGlobalPipes(new ValidationPipe())
 
