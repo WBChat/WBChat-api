af713dadc10937b33d99c255a5912261ee7747b2
getlost <sseniknik@gmail.com>
2024-05-28 01:03:05 +0300

    feat: files

--- src/core/Files/Files.module.ts
+++ src/core/Files/Files.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { FilesController } from './Files.controller';
 import { FilesService } from './Files.service'
 import { GridFsService } from '../GridFsService/GridFsService.service';
+import { AuthModule } from '../Auth/Auth.module';
 
 @Module({
-    imports: [],
+    imports: [forwardRef(() => AuthModule),],
     controllers: [FilesController],
     providers: [FilesService, GridFsService],
 })
